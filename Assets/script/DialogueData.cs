143bd30787f228a9ec1848589a5cab7eb277e838
MobG2001 <japanman1027@gmail.com>
2023-01-10 19:19:43 +0800

    平時作業

---
+++ Assets/script/DialogueData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace MobG
+{
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "MobG/Dialogue Data", fileName = "New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話者名稱"), TextArea(2, 10)]
+        public string[] dialogueContents;
+    }
+}
